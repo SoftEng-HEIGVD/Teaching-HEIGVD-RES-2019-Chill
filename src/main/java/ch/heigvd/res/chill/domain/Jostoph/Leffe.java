f336cd5df2a81b6b0228dcffc770d675383a676a
Christoph Rueff <christoph.rueff@gmail.com>
2019-02-19 10:49:53 +0100

    Add Leffe beer with test

---
+++ src/main/java/ch/heigvd/res/chill/domain/Jostoph/Leffe.java
@@ -0,0 +1,20 @@
+package ch.heigvd.res.chill.domain.Jostoph;
+
+import ch.heigvd.res.chill.domain.IProduct;
+
+import java.math.BigDecimal;
+
+public class Leffe implements IProduct{
+  public final static String NAME = "Leffe";
+  public final static BigDecimal PRICE = new BigDecimal(2.5);
+
+  @Override
+  public String getName() {
+    return NAME;
+  }
+
+  @Override
+  public BigDecimal getPrice() {
+    return PRICE;
+  }
+}
