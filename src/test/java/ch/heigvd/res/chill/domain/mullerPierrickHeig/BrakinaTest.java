35995328e3a2f05e60bd66898a0a58b37e95646d
Pierrick Muller <PierrickMuller@bitbucket.org>
2019-02-19 13:28:15 +0100

    Add Brakina with tests

---
+++ src/test/java/ch/heigvd/res/chill/domain/mullerPierrickHeig/BrakinaTest.java
@@ -0,0 +1,32 @@
+package ch.heigvd.res.chill.domain.mullerPierrickHeig;
+
+import ch.heigvd.res.chill.domain.Bartender;
+import ch.heigvd.res.chill.domain.wasadigi.Boxer;
+import ch.heigvd.res.chill.protocol.OrderRequest;
+import ch.heigvd.res.chill.protocol.OrderResponse;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class BrakinaTest {
+
+  @Test
+  void thePriceAndNameForBoxerShouldBeCorrect() {
+    Brakina beer = new Brakina();
+    assertEquals(beer.getName(), Brakina.NAME);
+    assertEquals(beer.getPrice(), Brakina.PRICE);
+  }
+
+  @Test
+  void aBartenderShouldAcceptAnOrderForBoxer() {
+    Bartender jane = new Bartender();
+    String productName = "ch.heigvd.res.chill.domain.mullerPierrickHeig.Brakina";
+    OrderRequest request = new OrderRequest(3, productName);
+    OrderResponse response = jane.order(request);
+    BigDecimal expectedTotalPrice = Brakina.PRICE.multiply(new BigDecimal(3));
+    assertEquals(expectedTotalPrice, response.getTotalPrice());
+  }
+
+}
