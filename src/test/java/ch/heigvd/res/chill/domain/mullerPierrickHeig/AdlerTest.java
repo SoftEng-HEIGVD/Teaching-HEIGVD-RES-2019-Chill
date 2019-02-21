558159c9ca30c19447f8c6b99ca0b2099be3073c
Pierrick Muller <PierrickMuller@bitbucket.org>
2019-02-21 09:08:55 +0100

    Add Adler with test

---
+++ src/test/java/ch/heigvd/res/chill/domain/mullerPierrickHeig/AdlerTest.java
@@ -0,0 +1,31 @@
+package ch.heigvd.res.chill.domain.mullerPierrickHeig;
+
+import ch.heigvd.res.chill.domain.Bartender;
+import ch.heigvd.res.chill.protocol.OrderRequest;
+import ch.heigvd.res.chill.protocol.OrderResponse;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class AdlerTest {
+
+  @Test
+  void thePriceAndNameForBoxerShouldBeCorrect() {
+    Adler beer = new Adler();
+    assertEquals(beer.getName(), Adler.NAME);
+    assertEquals(beer.getPrice(), Adler.PRICE);
+  }
+
+  @Test
+  void aBartenderShouldAcceptAnOrderForBoxer() {
+    Bartender jane = new Bartender();
+    String productName = "ch.heigvd.res.chill.domain.mullerPierrickHeig.Adler";
+    OrderRequest request = new OrderRequest(3, productName);
+    OrderResponse response = jane.order(request);
+    BigDecimal expectedTotalPrice = Adler.PRICE.multiply(new BigDecimal(3));
+    assertEquals(expectedTotalPrice, response.getTotalPrice());
+  }
+
+}
