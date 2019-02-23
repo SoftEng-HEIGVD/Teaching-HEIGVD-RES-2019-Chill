7c08a18cc96ad065dd12c1e0f10381c29a923303
RengokuryuuhonokaCrimsonFlame <julien.rod@heig-vd.ch>
2019-02-23 12:47:56 +0100

    First version of Artisanal beer

---
+++ src/test/java/ch/heigvd/res/chill/domain/wasadigi/ArtisanaleTest.java
@@ -0,0 +1,31 @@
+package ch.heigvd.res.chill.domain.wasadigi;
+
+import ch.heigvd.res.chill.domain.Bartender;
+import ch.heigvd.res.chill.protocol.OrderRequest;
+import ch.heigvd.res.chill.protocol.OrderResponse;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class ArtisanaleTest {
+
+    @Test
+    void thePriceAndTheNameForArtisanaleShouldBeCorrect(){
+        Artisanale beer = new Artisanale();
+        assertEquals(beer.getName(), Artisanale.NAME);
+        assertEquals(beer.getPrice(), Artisanale.PRICE);
+    }
+
+    @Test
+    void aBartenderShouldAcceptAnOrderForArtisanale(){
+        Bartender kokui = new Bartender();
+        String productName = "ch.heigvd.res.chill.domain.wasadigi.Artisanale";
+        OrderRequest request = new OrderRequest(42, productName);
+        OrderResponse response = kokui.order(request);
+        BigDecimal exceptedTotalPrice = Artisanale.PRICE.multiply(new BigDecimal(42));
+        assertEquals(exceptedTotalPrice,response.getTotalPrice());
+    }
+
+}
