c49c6ffe616d675cf580dadd9d710e1df8ea5139
BasileBotebol <basile.botebol@gmail.com>
2019-02-23 16:15:47 +0100

    Add maximator with test

--- src/test/java/ch/heigvd/res/chill/domain/BasileBotebol/MaximatorTest.java
+++ src/test/java/ch/heigvd/res/chill/domain/BasileBotebol/MaximatorTest.java
@@ -9,22 +9,22 @@ import java.math.BigDecimal;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class WestvleterenTest {
+class MaximatorTest {
 
     @Test
     void thePriceAndNameForWestvleterenShouldBeCorrect() {
-        Westvleteren beer = new Westvleteren();
-        assertEquals(beer.getName(), Westvleteren.NAME);
-        assertEquals(beer.getPrice(), Westvleteren.PRICE);
+        Maximator beer = new Maximator();
+        assertEquals(beer.getName(), Maximator.NAME);
+        assertEquals(beer.getPrice(), Maximator.PRICE);
     }
 
     @Test
     void aBartenderShouldAcceptAnOrderForWestvleteren() {
         Bartender jane = new Bartender();
-        String productName = "ch.heigvd.res.chill.domain.BasileBotebol.Westvleteren";
+        String productName = "ch.heigvd.res.chill.domain.BasileBotebol.Maximator";
         OrderRequest request = new OrderRequest(3, productName);
         OrderResponse response = jane.order(request);
-        BigDecimal expectedTotalPrice = Westvleteren.PRICE.multiply(new BigDecimal(3));
+        BigDecimal expectedTotalPrice = Maximator.PRICE.multiply(new BigDecimal(3));
         assertEquals(expectedTotalPrice, response.getTotalPrice());
     }
 
