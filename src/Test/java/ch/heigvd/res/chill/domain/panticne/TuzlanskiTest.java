732291f1b9fce6eb1c235fae6c29bb60ca0580fe
panticne <33542062+panticne@users.noreply.github.com>
2019-02-24 20:45:59 +0100

    Add Tuzlanski with test

---
+++ src/Test/java/ch/heigvd/res/chill/domain/panticne/TuzlanskiTest.java
@@ -0,0 +1,32 @@
+package ch.heigvd.res.chill.domain.panticne;
+
+import ch.heigvd.res.chill.domain.Bartender;
+import ch.heigvd.res.chill.domain.wasadigi.Boxer;
+import ch.heigvd.res.chill.protocol.OrderRequest;
+import ch.heigvd.res.chill.protocol.OrderResponse;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class DuvelTest {
+
+    @Test
+    void thePriceAndNameForDuvelShouldBeCorrect() {
+        Tuzlanski beer = new Tuzlanski();
+        assertEquals(beer.getName(), Tuzlanski.NAME);
+        assertEquals(beer.getPrice(), Tuzlanski.PRICE);
+    }
+
+    @Test
+    void aBartenderShouldAcceptAnOrderForDuvel() {
+        Bartender jane = new Bartender();
+        String productName = "ch.heigvd.res.chill.domain.panticne.Tuzlanski";
+        OrderRequest request = new OrderRequest(3, productName);
+        OrderResponse response = jane.order(request);
+        BigDecimal expectedTotalPrice = Tuzlanski.PRICE.multiply(new BigDecimal(3));
+        assertEquals(expectedTotalPrice, response.getTotalPrice());
+    }
+
+}
