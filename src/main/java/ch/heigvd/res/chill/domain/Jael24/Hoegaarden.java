b84297e63501aa11a8450db8c992463ca1e8cf43
Jael Dubey <jael.dubey@heig-vd.ch>
2019-02-24 17:37:23 +0100

    ajout Hoegaarden

--- src/main/java/ch/heigvd/res/chill/domain/Jael24/Hoegaarden.java
+++ src/main/java/ch/heigvd/res/chill/domain/Jael24/Hoegaarden.java
@@ -1,4 +1,20 @@
 package ch.heigvd.res.chill.domain.Jael24;
 
-public class Hoegaarden {
+import ch.heigvd.res.chill.domain.IProduct;
+import java.math.BigDecimal;
+
+public class Hoegaarden implements IProduct {
+
+    public final static String NAME = "Hoegaarden";
+    public final static BigDecimal PRICE = new BigDecimal(2.0);
+
+    @Override
+    public String getName() {
+        return NAME;
+    }
+
+    @Override
+    public BigDecimal getPrice() {
+        return PRICE;
+    }
 }
