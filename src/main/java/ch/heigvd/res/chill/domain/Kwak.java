9dabe918176e73ffce749462f6875db953524a6a
JulienHuguet <julien.huguet@heig-vd.ch>
2019-02-24 22:24:52 +0100

    Add Kwak

---
+++ src/main/java/ch/heigvd/res/chill/domain/Kwak.java
@@ -0,0 +1,17 @@
+package ch.heigvd.res.chill.domain;
+import ch.heigvd.res.chill.domain.IProduct;
+import java.math.BigDecimal;
+
+public class Kwak implements IProduct{
+    public final static String NAME = "Kwak";
+    public final static BigDecimal PRICE = new BigDecimal(2.0);
+
+    @Override
+    public String getName(){
+        return NAME;
+    }
+    @Override
+    public BigDecimal getPrice(){
+        return PRICE;
+    }
+}
