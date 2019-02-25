a771d687cd5b584ecd0d5db0edfafdeb71287346
volkan05 <volkan.sutcu@heig-vd.ch>
2019-02-25 14:15:13 +0100

    Adding Cardinal

---
+++ src/Test/java/ch/heigvd/res/chill/domain/volkan05/CardinalTest.java
@@ -0,0 +1,31 @@
+package ch.heigvd.res.chill.domain.volkan05;
+
+import ch.heigvd.res.chill.domain.Bartender;
+import ch.heigvd.res.chill.protocol.OrderRequest;
+import ch.heigvd.res.chill.protocol.OrderResponse;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class CardinalTest {
+
+  @Test
+  void thePriceAndNameForBeer1664ShouldBeCorrect() {
+    Cardinal beer = new Cardinal();
+    assertEquals(beer.getName(), Cardinal.NAME);
+    assertEquals(beer.getPrice(), Cardinal.PRICE);
+  }
+
+  @Test
+  void aBartenderShouldAcceptAnOrderForCardinal() {
+    Bartender jane = new Bartender();
+    String productName = "ch.heigvd.res.chill.domain.volkan05.Cardinal";
+    OrderRequest request = new OrderRequest(3, productName);
+    OrderResponse response = jane.order(request);
+    BigDecimal expectedTotalPrice = Cardinal.PRICE.multiply(new BigDecimal(3));
+    assertEquals(expectedTotalPrice, response.getTotalPrice());
+  }
+
+}
