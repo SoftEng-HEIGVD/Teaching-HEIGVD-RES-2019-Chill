95d88fa053c6917cb573bbfaa6e01700497a5e3e
yoannsim <yoann.simonet@heig-vd.ch>
2019-02-25 10:47:59 +0100

    sake2

--- src/main/java/ch/heigvd/res/chill/domain/yoannsim/JujuSamaSake.java
+++ src/main/java/ch/heigvd/res/chill/domain/yoannsim/JujuSamaSake.java
@@ -3,10 +3,10 @@ import ch.heigvd.res.chill.domain.IProduct;
 
 import java.math.BigDecimal;
 
-public class DocteurGabs implements IProduct {
+public class JujuSamaSake implements IProduct {
 
-        public final static String NAME = "DocteurGabs";
-        public final static BigDecimal PRICE = new BigDecimal(4);
+        public final static String NAME = "JujuSamaSake";
+        public final static BigDecimal PRICE = new BigDecimal(100);
 
         @Override
         public String getName() {
