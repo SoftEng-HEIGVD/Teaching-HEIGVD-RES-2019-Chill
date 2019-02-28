4597c30835992aa9cffca3961be6f6e039a7abca
BasileBotebol <basile.botebol@gmail.com>
2019-02-28 09:24:22 +0100

    Add Thirdbeer with test

---
+++ src/test/java/ch/heigvd/res/chill/domain/BasileBotebol/ThirdbeerTest.java
@@ -0,0 +1,31 @@
+package ch.heigvd.res.chill.domain.BasileBotebol;
+
+import ch.heigvd.res.chill.domain.Bartender;
+import ch.heigvd.res.chill.protocol.OrderRequest;
+import ch.heigvd.res.chill.protocol.OrderResponse;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class ThirdbeerTest {
+
+    @Test
+    void thePriceAndNameForThirdbeerShouldBeCorrect() {
+        Thirdbeer beer = new Thirdbeer();
+        assertEquals(beer.getName(), Thirdbeer.NAME);
+        assertEquals(beer.getPrice(), Thirdbeer.PRICE);
+    }
+
+    @Test
+    void aBartenderShouldAcceptAnOrderForThirdbeer() {
+        Bartender jane = new Bartender();
+        String productName = "ch.heigvd.res.chill.domain.BasileBotebol.Thirdbeer";
+        OrderRequest request = new OrderRequest(3, productName);
+        OrderResponse response = jane.order(request);
+        BigDecimal expectedTotalPrice = Thirdbeer.PRICE.multiply(new BigDecimal(3));
+        assertEquals(expectedTotalPrice, response.getTotalPrice());
+    }
+
+}
