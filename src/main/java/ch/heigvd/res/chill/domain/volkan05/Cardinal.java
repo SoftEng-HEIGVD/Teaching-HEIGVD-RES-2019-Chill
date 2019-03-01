4e41b8ea170f15535cb146f860a3e300286928e0
Olivier Liechti <olivier.liechti@wasabi-tech.com>
2019-03-01 11:32:50 +0100

    Update Cardinal price

--- src/main/java/ch/heigvd/res/chill/domain/volkan05/Cardinal.java
+++ src/main/java/ch/heigvd/res/chill/domain/volkan05/Cardinal.java
@@ -7,7 +7,7 @@ import java.math.BigDecimal;
 public class Cardinal implements IProduct {
 
   public final static String NAME = "Cardinal";
-  public final static BigDecimal PRICE = new BigDecimal(2.0);
+  public final static BigDecimal PRICE = new BigDecimal(2.1);
 
   @Override
   public String getName() {
