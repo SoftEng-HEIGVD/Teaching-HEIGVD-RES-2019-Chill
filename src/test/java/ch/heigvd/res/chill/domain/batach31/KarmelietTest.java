fe50c59b2638dc197bb803e3dd82d7a8c38a7e22
unknown <batach31@hotmail.com>
2019-03-01 18:55:28 +0100

    Added Karmeliet feature with tests

---
+++ src/test/java/ch/heigvd/res/chill/domain/batach31/KarmelietTest.java
@@ -0,0 +1,31 @@
+package ch.heigvd.res.chill.domain.batach31;
+
+import ch.heigvd.res.chill.domain.Bartender;
+import ch.heigvd.res.chill.protocol.OrderRequest;
+import ch.heigvd.res.chill.protocol.OrderResponse;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class KarmelietTest {
+
+    @Test
+    void thePriceAndNameForKarmelietShouldBeCorrect() {
+        Karmeliet beer = new Karmeliet();
+        assertEquals(beer.getName(), Karmeliet.NAME);
+        assertEquals(beer.getPrice(), Karmeliet.PRICE);
+    }
+
+    @Test
+    void aBartenderShouldAcceptAnOrderForKarmeliet() {
+        Bartender jane = new Bartender();
+        String productName = "ch.heigvd.res.chill.domain.batach31.Karmeliet";
+        OrderRequest request = new OrderRequest(3, productName);
+        OrderResponse response = jane.order(request);
+        BigDecimal expectedTotalPrice = Karmeliet.PRICE.multiply(new BigDecimal(3));
+        assertEquals(expectedTotalPrice, response.getTotalPrice());
+    }
+
+}
