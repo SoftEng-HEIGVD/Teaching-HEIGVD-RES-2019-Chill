ba7b33acc59ea0d1299a5b3ce5d61f2b03ee4681
Gaétan <gaetan.zwick@gmail.com>
2019-03-01 13:43:39 +0100

    added grimbergen with test

---
+++ src/main/java/ch/heigvd/res/chill/domain/ga3tan/Grimbergen.java
@@ -0,0 +1,21 @@
+package ch.heigvd.res.chill.domain.ga3tan;
+
+import ch.heigvd.res.chill.domain.IProduct;
+
+import javax.naming.Name;
+import java.math.BigDecimal;
+
+public class Grimbergen implements IProduct {
+    public final static String NAME = "Grimbergen";
+    public final static BigDecimal PRICE = new BigDecimal(3);
+
+    @Override
+    public String getName() {
+        return NAME;
+    }
+
+    @Override
+    public BigDecimal getPrice() {
+        return PRICE;
+    }
+}
