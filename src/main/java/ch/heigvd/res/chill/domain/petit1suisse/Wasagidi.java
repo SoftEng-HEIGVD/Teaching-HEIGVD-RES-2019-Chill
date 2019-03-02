94647221c19eb93b330a700c07a578a063ee1252
petit1suisse <alexandre.gabrielli@users.noreply.github.com>
2019-03-02 13:07:56 +0100

    add Wasagidi

---
+++ src/main/java/ch/heigvd/res/chill/domain/petit1suisse/Wasagidi.java
@@ -0,0 +1,21 @@
+package ch.heigvd.res.chill.domain.petit1suisse;
+
+import ch.heigvd.res.chill.domain.IProduct;
+
+import java.math.BigDecimal;
+
+public class Wasagidi implements IProduct {
+    public final static String NAME = "Wasagidi";
+    public final static BigDecimal PRICE = new BigDecimal((Double.MAX_VALUE));
+
+    @Override
+    public String getName() {
+        return NAME;
+    }
+
+    @Override
+    public BigDecimal getPrice() {
+        return PRICE;
+    }
+
+}
