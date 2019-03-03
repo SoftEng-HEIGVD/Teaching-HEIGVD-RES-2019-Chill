4d4077271020652115f43191870328f3e043003e
panticne <33542062+panticne@users.noreply.github.com>
2019-03-03 14:49:47 +0100

    Update ImaginaryBeerTest.java

--- src/test/java/ch/heigvd/res/chill/domain/panticne/ImaginaryBeerTest.java
+++ src/test/java/ch/heigvd/res/chill/domain/panticne/ImaginaryBeerTest.java
@@ -10,7 +10,7 @@ import java.math.BigDecimal;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class ImaginaryBeer {
+class ImaginaryBeerTest {
 
     @Test
     void thePriceAndNameForImaginaryBeerShouldBeCorrect() {
