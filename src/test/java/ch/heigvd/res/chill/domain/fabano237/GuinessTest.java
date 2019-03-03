7ead6063afa713e44390ab65ddc12c59047b7e41
ericafabrice <fabrice.mbassinguema@heig-vd.ch>
2019-03-03 05:47:04 +0100

    the clean version beer Guiness

---
+++ src/test/java/ch/heigvd/res/chill/domain/fabano237/GuinessTest.java
@@ -0,0 +1,61 @@
+package ch.heigvd.res.chill.domain.fabano237;
+
+
+
+import ch.heigvd.res.chill.domain.Bartender;
+
+import ch.heigvd.res.chill.protocol.OrderRequest;
+
+import ch.heigvd.res.chill.protocol.OrderResponse;
+
+import org.junit.jupiter.api.Test;
+
+
+
+import java.math.BigDecimal;
+
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+
+public class GuinessTest{
+
+
+
+    @Test
+
+    void thePriceAndNameForGuinessShouldBeCorrect() {
+
+        Guiness beer = new Guiness();
+
+        assertEquals(beer.getName(), Guiness.NAME);
+
+        assertEquals(beer.getPrice(), Guiness.PRICE);
+
+    }
+
+
+
+    @Test
+
+    void aBartenderShouldAcceptAnOrderForGuiness() {
+
+        Bartender jane = new Bartender();
+
+        String productName = "ch.heigvd.res.chill.domain.fabano237.Guiness";
+
+        OrderRequest request = new OrderRequest(3, productName);
+
+        OrderResponse response = jane.order(request);
+
+        BigDecimal expectedTotalPrice = Guiness.PRICE.multiply(new BigDecimal(3));
+
+        assertEquals(expectedTotalPrice, response.getTotalPrice());
+
+    }
+
+
+
+}
